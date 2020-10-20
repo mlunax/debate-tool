e055727fba257e56b518efee908dcf91b611768b
lunax <mlunaelumen@gmail.com>
2020-10-20 16:16:18 +0200

    additional input field next to name/only view

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const peopleConfigList = (event) => {
 	const howManyAgree = parseInt(document.getElementById("howManyPeopleAgree").value)
 	const howManyDisagree = parseInt(document.getElementById("howManyPeopleDisagree").value)
+	const time = document.getElementById("time").value
+	const blockTime = document.getElementById("blockTime").value
 	let html = "";
 	let howMany = howManyDisagree;
 	if (howManyAgree > howManyDisagree) {
@@ -9,26 +11,42 @@ const peopleConfigList = (event) => {
 	for (let i = 1; i <= howMany; i++) {
 		html += `<div class="row">`
 		html += ` <div class="six columns">`
+		html += ` <div class="six columns">`
 		if (howManyAgree >= i) {
 			html += `<label for="name-${i}">Imie osoby nr. ${i}</label>
                 		<input class="u-full-width" type="text" placeholder="Imie" id="name-${i}"
-                		onkeydown="if (event.keyCode === 13) document.getElementById('name-dis-${i}').focus()">`
+						onkeydown="if (event.keyCode === 13) document.getElementById('name-dis-${i}').focus()">`
+			html += `</div>`
+			html += `<div class="six columns">
+			<label>Czas</label>
+			<input class="u-full-width" type="text" placeholder="time" value="${time}">`
 		} else {
 			html += `<label style="visibility: hidden">Imie osoby nr. ${i}</label>`
 		}
 		html += `</div>`
+		html += `</div>`
+		html += ` <div class="six columns">`
 		html += ` <div class="six columns">`
 		if (howManyDisagree > i) {
 			html += `<label for="name-dis-${i}">Imie osoby nr. ${i}</label>
                 		<input class="u-full-width" type="text" placeholder="Imie" id="name-dis-${i}"
-                		onkeydown="if (event.keyCode === 13) document.getElementById('name-${i + 1}').focus()">`
+						onkeydown="if (event.keyCode === 13) document.getElementById('name-${i + 1}').focus()">`
+			html += `</div>`
+			html += `<div class="six columns">
+			<label>Czas</label>
+			<input class="u-full-width" type="text" placeholder="time" value="${time}">`
 		} else if (howManyDisagree === i) {
 			html += `<label for="name-dis-${i}">Imie osoby nr. ${i}</label>
                 		<input class="u-full-width" type="text" placeholder="Imie" id="name-dis-${i}"
-                		onkeydown="if (event.keyCode === 13) document.getElementById('submit').focus()">`
+						onkeydown="if (event.keyCode === 13) document.getElementById('submit').focus()">`
+			html += `</div>`
+			html += `<div class="six columns">
+			<label>Czas</label>
+			<input class="u-full-width" type="text" placeholder="time" value="${time}">`
 		} else {
 			html += `<label style="visibility: hidden">Imie osoby nr. ${i}</label>`
 		}
+		html += `</div>`
 		html += `</div>`
 		html += `</div>`
 	}
